85efa9f560010614fbdf3df814e60180c679f5f5
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-02-13 18:40:25 +0300

    Updates

---
+++ precache-manifest.373e0341a325f770dd1fbb866268b4bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a2fd3acd7e708ca6407a5f994b5074",
+    "url": "/TracksProject/index.html"
+  },
+  {
+    "revision": "4eff2a6a5c819b97e496",
+    "url": "/TracksProject/static/css/main.c1c73df8.chunk.css"
+  },
+  {
+    "revision": "8d0370b3eafe50203cf8",
+    "url": "/TracksProject/static/js/2.274fd634.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TracksProject/static/js/2.274fd634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eff2a6a5c819b97e496",
+    "url": "/TracksProject/static/js/main.b3b53484.chunk.js"
+  },
+  {
+    "revision": "77fdc68d82c32b5282f8",
+    "url": "/TracksProject/static/js/runtime-main.27889b72.js"
+  }
+]);
